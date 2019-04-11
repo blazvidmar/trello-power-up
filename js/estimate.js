adc459c332db25ad597a5c92b721f4461252a3d7
Blaž Vidmar <mr.blazvidmar@gmail.com>
2019-04-11 18:57:09 +0200

    fix

--- js/estimate.js
+++ js/estimate.js
@@ -2,13 +2,14 @@
 var t = TrelloPowerUp.iframe();
 
 window.estimate.addEventListener('submit', function(event){
-    event.preventDefault();
-    return t.set('card', 'shared', 'estimate', window.estimateSize.value)
-    .then(function(){
-        t.closePopup();
-    });
+  // Stop the browser trying to submit the form itself.
+  event.preventDefault();
+  return t.set('card', 'shared', 'estimate', window.estimateSize.value)
+  .then(function(){
+    t.closePopup();
+  });
 });
 
 t.render(function(){
-    t.sizeTo('#content').done();
+  t.sizeTo('#estimate').done();
 });
