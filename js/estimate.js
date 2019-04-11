66bf523ed34000dbf9988b499844a60b8423f78e
Blaž Vidmar <mr.blazvidmar@gmail.com>
2019-04-11 18:54:12 +0200

    eventListener

--- js/estimate.js
+++ js/estimate.js
@@ -1,6 +1,14 @@
 
 var t = TrelloPowerUp.iframe();
 
+window.estimate.addEventListener('submit', function(event){
+    event.preventDefault();
+    return t.set('card', 'shared', 'estimate', window.estimateSize.value)
+    .then(function(){
+        t.closePopup();
+    });
+});
+
 t.render(function(){
     t.sizeTo('#content').done();
 });
